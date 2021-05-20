6a34c80125ec8fcccb5984f680fd97543ac2ec96
philippe-blanquier <philippe.blanquier@gmail.com>
2021-05-20 19:59:17 +0200

    amélioration du panier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,162 +1,350 @@
 /*
  * JavaScript file
  */
-const tagItemId="id";
-let totalPrice= 0;
-function entryPoint( parentIdName,  totalPriceIdName)
+const prefixSelect= "select-",
+      prefixPrice= "price-",
+      prefixNombre= "nombre-";
+const itemId= "id",
+      itemTitle= "title",
+      itemDescription= "description",
+      itemGenre= "genre",
+      itemImage= "image",
+      itemPrice= "price",
+      itemConsoleType= "console",
+      itemQuantity= "Qty";
+const euroFormatter = new Intl.NumberFormat('it-IT', {
+        style: 'currency',
+        currency: 'EUR'
+        })
+      
+let totalPrice= 0,
+    totalItem= 0;
+let totalpriceId, 
+    totalItemId,
+    savedSelectedList,
+    savedBodyIdName,
+    savedItemCatalog;
+
+function entryPoint( parentIdName,  totalPriceIdName, totalItemName)
 {
     let myCatalog=[];
     myCatalog[0]=   {
                     "id": "001",
                     "title": "Mario Bross",
-                    "descr": "un plombier roulant dans un kart",
+                    "description": "un plombier roulant dans un kart",
                     "genre": "course",
                     "image": "Smile.png",
                     "price": "3.14",
-                    "console": "XBoX"
+                    "console": "XBoX",
+                    "Qty" : 1
                     };
     myCatalog[1]=   {
                     "id": "002",
                     "title": "Tweety & Sylvester",
-                    "descr": "Titi et gros minet se ballade en foret",
+                    "description": "Titi et gros minet se ballade en foret",
                     "genre": "rpg",
                     "image": "Worried.png",
                     "price": "1.23",
-                    "console": "PlayStation"
+                    "console": "PlayStation",
+                    "Qty" : 2
                     };
     myCatalog[2]=   {
                     "id": "003",
-                    "title": "Kun Fu Fu",
-                    "descr": "aie, ouille...",
-                    "genre": "rpgbagarre",
+                    "title": "Kung Fu Fu",
+                    "description": "aie, ouille...",
+                    "genre": "Combat",
                     "image": "Worried.png",
                     "price": "3.00",
-                    "console": "PlayStation"
+                    "console": "PlayStation",
+                    "Qty" : 3
                     };
-        let myIdList=[ "001","002"]
-    //displayCaddie( parentIdName, myIdList, myCatalog);
-    //let caddieFooterId= document.getElementById(caddieFooterIdName);
-    //totalPriceUpdate( caddieFooterId, 0);
-    fillCaddieBoddie( parentIdName, totalPriceIdName, myIdList, myCatalog)
+    let myIdList=[ "003","002"]
+   fillCaddieBoddie( parentIdName, totalPriceIdName, totalItemName, myIdList, myCatalog)
 }
-function totalPriceUpdate( totalpriceId, deltaPrice)
+function increaseTotalItemNb()
 {
-    totalPrice+= parseFloat(deltaPrice);
-    totalpriceId.innerText= `Total= ${totalPrice} euros`;
+    totalItem += 1;
+    totalItemId.innerText= totalItem;
 }
-function displayOneElement( parentId, refId, title, genre, imagePath, price, consoleType)
+function decreaseTotalItemNb()
 {
-    console.info(parentId);
+    totalItem -= 1;
+    if (totalItem < 0)
+    {
+        totalItem= 0;
+    }
+    totalItemId.innerText= totalItem;
+}
+function getItemParam( itemRefName, paramType )
+{
+    let answer= null;
+    savedItemCatalog.forEach( itemIdx => {
+        if (itemIdx.id == itemRefName)
+        {
+            // Yeap !
+            switch(paramType)
+            {
+                case itemId: answer= itemIdx.id; break;
+                case itemTitle: answer= itemIdx.title; break;
+                case itemDescription: answer= itemIdx.description; break;
+                case itemGenre: answer= itemIdx.genre; break;
+                case itemImage: answer= itemIdx.image; break;
+                case itemPrice: answer= parseFloat(itemIdx.price); break;
+                case itemConsoleType: answer= itemIdx.console; break;
+                case itemQuantity: answer= parseInt(itemIdx.Qty); break;
+                default: answer= null;
+            }
+        }
+     })
+    return answer;
+}
+function increaseOneItem( itemRefName )
+{
+    // Can't sell more item than we have in stock.
+    let maxAvailableItem= getItemParam( itemRefName, itemQuantity);
+    console.log(`increaseOneItem( ${itemRefName} ) = ${maxAvailableItem}`)
+    // Update the item number
+    let itemfullName= buildItemIdNombre(itemRefName);
+    let itemId= document.getElementById(itemfullName);
+    if (parseInt(itemId.innerText) >= maxAvailableItem)
+    {
+        let itemIdTitle= getItemParam( itemRefName, itemTitle);
+        let itemIdConsole= getItemParam( itemRefName, itemConsoleType);
+        alert(`On voudrait bien vous en vendre plus mais on n'en n'a pas assez de "${itemIdTitle}" pour "${itemIdConsole}" en stock. Désolé...`);
+    }
+    else
+    {
+        itemId.innerText= parseInt(itemId.innerText) +1;
+        // Update the total basket price
+        itemfullName= buildItemIdPrice(itemRefName);
+        itemId= document.getElementById(`${itemfullName}`);
+        totalPriceUpdate(itemId.innerText);
+        increaseTotalItemNb();
+    }
+}
+function decreaseOneItem( itemRefName )
+{
+    // Update the item number
+    let itemfullName= buildItemIdNombre(itemRefName);
+    let itemId= document.getElementById(itemfullName);
+    if (parseInt(itemId.innerText) == 1)
+    {
+        // Remove the item
+        removeSelectedItem(buildItemIdName( itemRefName ));
+    }
+    else
+    {
+        // Update the counters
+        itemId.innerText= parseInt(itemId.innerText) -1;
+        // Update the total basket price
+        itemfullName= buildItemIdPrice(itemRefName);
+        itemId= document.getElementById(`${itemfullName}`);
+        totalPriceUpdate(`-${itemId.innerText}`);
+        decreaseTotalItemNb();
+    }
+}
+function totalPriceUpdate( deltaPrice)
+{
+    let deltaPriceValue= parseFloat(deltaPrice);
+    if (deltaPriceValue >= 0)
+    {
+        // Add a new price
+        totalPrice+= deltaPriceValue;
+    }
+    else
+    {
+        // Remove a price   ;-)
+        if (totalPrice >= deltaPriceValue)
+        {
+            totalPrice+= deltaPriceValue;
+        }
+    }
+    totalPriceId.innerText= euroFormatter.format(parseFloat(totalPrice));
+}
+function removeSelectedItem( itemRefName)
+{
+    // Update the selected item list
+    let originalitemRefName= itemRefName.replace(prefixSelect,"");
+    for (let idx=0; idx < savedSelectedList.length; idx++)
+    {
+        if (savedSelectedList[idx] == originalitemRefName)
+        {
+            // Item to be removed found !
+            let priceValue= 0;
+            // Search its price
+            savedItemCatalog.forEach( itemIdx => {
+                if (itemIdx.id == originalitemRefName)
+                {
+                    priceValue= parseFloat(`-${itemIdx.price}`);
+                }
+            });
+            totalPriceUpdate( priceValue)
+            // Remove the selected item from display
+            let itemId= document.getElementById( buildItemIdName (itemRefName));
+            itemId.remove();
+            // Update the selected list
+            savedSelectedList.splice(idx,1);
+            // Snif ! One less item...
+            decreaseTotalItemNb();
+            // Done
+            break;
+        }
+    }
+}
+function buildItemIdNombre( refId )
+{
+    return `${prefixNombre}${refId}`
+}
+function buildItemIdName( refId )
+{
+    return `${prefixSelect}${refId}`
+}
+function buildItemIdPrice( refId )
+{
+    return `${prefixPrice}${refId}`
+}
+function removeBasketContent()
+{
+    let answer= confirm( `Abondonnez vous votre commande  ?` )
+    if (answer == true)
+    {
+        let copiedList= [...savedSelectedList];
+        copiedList.forEach( item => {
+            removeSelectedItem( buildItemIdName( item ));
+            });
+    }
+}
+function closeModalWindow()
+{
+    let modalId= document.getElementById("basket");
+    console.info(modalId);
+}
+function validBasketContent()
+{
+    alert("Votre commande a bien été prise en compte");
+    closeModalWindow();
+}
+function displaySelectedItem( parentId, refId, title, genre, imagePath, price, consoleType)
+{
     let mainItemId= document.createElement("div");
-    mainItemId.className= "row";
+    mainItemId.className= "row item-info";
+    mainItemId.id= buildItemIdName( refId );
     parentId.appendChild(mainItemId);
     // Create the first column
-    let colImg= document.createElement("div");
-    colImg.className= "col";
-    mainItemId.appendChild(colImg);
-    // Insert into the first column the image
+    let colId= document.createElement("div");
+    colId.className= "col-2";
+    mainItemId.appendChild(colId);
+    // Add image
     let imgId=  document.createElement("img");
     imgId.src= imagePath;
     imgId.alt= "poster";
     imgId.width= "200";
     imgId.heigth= "auto";
     imgId.title= title;
-    colImg.appendChild(imgId);
+    imgId.style= "margin: 3px";
+    colId.appendChild(imgId);
     // Create the second column
-    let colInfo= document.createElement("div");
-    colInfo.className= "col";
-    mainItemId.appendChild(colInfo);
-    // Insert into the second column with the RefId
+    colId= document.createElement("div");
+    colId.className= "col-4";
+    mainItemId.appendChild(colId);
+    // Add the RefId
     let rowId= document.createElement("div");
     rowId.className= "row";
     rowId.innerText= `Ref: ${refId}`;
-    colInfo.appendChild(rowId);
-    // Insert into the second column with the Title
+    colId.appendChild(rowId);
+    // Add the Title
     rowId= document.createElement("div");
     rowId.className= "row";
     rowId.innerText= `Title: ${title}`;
-    colInfo.appendChild(rowId);
-    // Insert into the second column with the Genre
+    colId.appendChild(rowId);
+    // Add the Genre
     rowId= document.createElement("div");
     rowId.className= "row";
     rowId.innerText= `Genre: ${genre}`;
-    colInfo.appendChild(rowId);
-    // Insert into the second column with the Type
+    colId.appendChild(rowId);
+    // Add the Type
     rowId= document.createElement("div");
     rowId.className= "row";
     rowId.innerText= `Type: ${consoleType}`;
-    colInfo.appendChild(rowId);
-    // Insert into the second column with the price
+    colId.appendChild(rowId);
+    // Create the third column
+    colId= document.createElement("div");
+    colId.className= "col-2";
+    mainItemId.appendChild(colId);
+    // Add the quantity
     rowId= document.createElement("div");
     rowId.className= "row";
-    rowId.innerText= `Prix: ${price} euros`;
-    colInfo.appendChild(rowId);
+    rowId.innerText= "Nombre";
+    colId.appendChild(rowId);
+    rowId= document.createElement("div");
+    rowId.className= "row";
+    rowId.id= buildItemIdNombre( refId );
+    rowId.innerText= "1";
+    colId.appendChild(rowId);
+    rowId= document.createElement("div");
+    rowId.className= "row";
+    colId.appendChild(rowId);
+    let button= document.createElement("button");
+    button.className= "btn";
+    button.innerText= "+"
+    button.setAttribute("onclick", `increaseOneItem("${refId}")`);
+    rowId.appendChild(button);
+    button= document.createElement("button");
+    button.className= "btn";
+    button.innerText= "-"
+    button.setAttribute("onclick", `decreaseOneItem("${refId}")`);
+    rowId.appendChild(button);
+     // Create the forth column
+    colId= document.createElement("div");
+    colId.className= "col-2";
+    mainItemId.appendChild(colId);
+    // Add the unitary price
+    rowId= document.createElement("div");
+    rowId.className= "row text-center";
+    rowId.innerText= "Prix unitaire"
+    colId.appendChild(rowId);
+    rowId= document.createElement("div");
+    rowId.className= "row";
+    rowId.id= buildItemIdPrice(refId);
+    rowId.innerText= euroFormatter.format(parseFloat(price));
+    colId.appendChild(rowId);
+    // Create the fith column
+    colId= document.createElement("div");
+    colId.className= "col-2";
+    mainItemId.appendChild(colId);
+    // Add a trashbin
+    imgId= document.createElement("img");
+    imgId.src= "assets/images/icon_empty_trash.png";
+    imgId.alt= "trashbin";
+    imgId.width= "30";
+    imgId.heigth= "auto";
+    imgId.title= "trashbin";
+    imgId.style= "margin: 3px, height: auto";
+    imgId.setAttribute("onclick", `removeSelectedItem("${refId}")`);
+    colId.appendChild(imgId);
+    // And one more item into the basket !
+    increaseTotalItemNb();
 }
-function fillCaddieBoddie( bodyTagIdName, totalPriceIdName, id_list, GoodInfo)
+function fillCaddieBoddie( bodyIdName, totalPriceIdName, totalItemName, selectedItemList, allItemList)
 {
-    if (id_list.length <= 0) return;
-    console.log( `fillCaddieBoddie( ${bodyTagIdName}, ${totalPriceIdName}, ...)`)
-    let bodyTagId= document.getElementById(bodyTagIdName);
-    let totalPriceId= document.getElementById(totalPriceIdName);
+    if (selectedItemList.length <= 0) return;
+    savedSelectedList= selectedItemList;
+    savedBodyIdName= bodyIdName;
+    savedItemCatalog= [...allItemList];
+    let bodyTagId= document.getElementById(bodyIdName);
+    totalPriceId= document.getElementById(totalPriceIdName);
+    totalItemId= document.getElementById(totalItemName);
     // For each selected item...
-    id_list.forEach( elementIdx => {
+    selectedItemList.forEach( elementIdx => {
         // Search the corresponding items
-        GoodInfo.forEach( itemIdx => {
+        savedItemCatalog.forEach( itemIdx => {
             if (itemIdx.id == elementIdx)
                 {
                     // Yeap ! Display this one...
-                    displayOneElement( bodyTagId, itemIdx.id, itemIdx.title, itemIdx.genre, itemIdx.image, itemIdx.price, itemIdx.console);
-                    totalPriceUpdate( totalPriceId, itemIdx.price);
+                    displaySelectedItem( bodyTagId, itemIdx.id, itemIdx.title, itemIdx.genre, itemIdx.image, itemIdx.price, itemIdx.console);
+                    totalPriceUpdate( itemIdx.price);
                 }
             })
         });
+    console.info(bodyTagId);
 }
-function displayCaddie( parentIdName, id_list, GoodInfo)
-{
-    // Only one caddie window
-    let previouscaddieModalWindowId= document.getElementById("caddieModalWindowId");
-    if (previouscaddieModalWindowId != null)
-    {
-        previouscaddieModalWindowId.remove();      
-    }
-    // Build the caddie modal window
-    let parentId= document.getElementById(parentIdName),
-        caddieModalWindowId= document.createElement("div");
-    caddieModalWindowId.className= "modal hide fade";
-    // data-toggle="modal"
-    caddieModalWindowId.id= "caddieModalWindowId";
-    parentId.appendChild(caddieModalWindowId);
-    // Add the content --> modal
-    let caddieContentId= document.createElement("div");
-    caddieContentId.className= "modal-content";
-    caddieModalWindowId.appendChild(caddieContentId);
-    // Add the header --> content
-    let caddieHeaderId= document.createElement("div");
-    caddieHeaderId.className= "modal-header";
-    caddieHeaderId.innerText="Votre pannier";
-    caddieContentId.appendChild(caddieHeaderId);
-    // Add body --> content
-    let caddieBoddyId= document.createElement("div");
-    caddieBoddyId.className= "modal-boddy";
-    caddieContentId.appendChild(caddieBoddyId);
-    // Add the footer --> content
-    let caddieFooterId= document.createElement("div");
-    caddieFooterId.className= "modal-footer";
-    caddieFooterId.style= "text-align: center;";
-    totalPriceUpdate( caddieFooterId, 0);
-    caddieContentId.appendChild(caddieFooterId);
-    // Add a validate & cancel buttons to the footer
-    let validateId= document.createElement("span");
-    validateId.className="validate-button";
-    validateId.innerText= "Valider";
-    caddieFooterId.appendChild(validateId);
-    let caddieCancelId= document.createElement("span");
-    caddieCancelId.className="cancel-button";
-    caddieCancelId.innerText= "Annuler";
-    caddieFooterId.appendChild(caddieCancelId);
-    // Fill the caddie display
-    fillCaddieBoddie( caddieBoddyId, caddieFooterId, id_list, GoodInfo);
-    // Display the modal window
-    //? caddieModalWindowId.classList.toggle("show-modal");
-    caddieModalWindowId.style.display= "block";
-    }
